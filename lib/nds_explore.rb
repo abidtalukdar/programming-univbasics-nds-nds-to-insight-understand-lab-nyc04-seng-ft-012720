1e9e722685ee0f49bbe2fb4afb80ba56361a42ae
Abid Talukdar <abiditalukdar@gmail.com>
2020-01-15 17:00:33 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -12,11 +12,11 @@ end
 
 def print_first_directors_movie_titles
   row_index = 0
-  while row_index < nds.length do 
+  while row_index < pretty.length do 
     column_index = 0
-    while column_index < nds[row_index].length do
+    while column_index < pretty[row_index].length do
       depth_index = 0 
-      while depth_index < nds[row_index][column_index].length do
+      while depth_index < pretty[row_index][column_index].length do
         puts nds[row_index][column_index][depth_index]
       end  
     end
